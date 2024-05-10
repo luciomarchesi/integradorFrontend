1215d37eb594ad0407f30c7491b3131cfc96e564
= <=>
2024-05-10 15:12:25 -0300

    se agrega link a user.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-const registo = document.getElementById("registro");
+//const registo = document.getElementById("registro");
 const user = JSON.parse(localStorage.getItem("user"));
+const lista = document.getElementById("miLista");
+const nuevoElemento = document.createElement("li");
+const nuevobtn = document.createElement("button");
+var nuevoEnlace = document.createElement("a");
+nuevoEnlace.setAttribute("href", "./pages/user.html");
 
-console.log(user)
-if(localStorage.getItem("user")){
-    registo.innerHTML = user.email_user; 
-    registo.href = "user.html";
+console.log(user);
+if (localStorage.getItem("user")) {
+  // registo.innerHTML = user.email_user;
+  // registo.href = "user.html";
+  nuevoEnlace.textContent = user.email_user;
+  nuevoElemento.appendChild(nuevoEnlace);
+  nuevoElemento.classList.add("--bs-primary-text-emphasis");
+  nuevoElemento.classList.add("nav-item");
+  nuevoElemento.classList.add("nav-link");
+  nuevoElemento.classList.add("mx-2");
+  lista.appendChild(nuevoElemento);
 }
