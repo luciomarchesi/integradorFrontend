82703603e867b29ab81ceeb9db262e3d82503d7e
= <=>
2024-05-13 16:06:23 -0300

    se completan con expresiones regulares las validaciones

--- js/registrarse.js
+++ js/registrarse.js
@@ -1,13 +1,31 @@
-const form = document.getElementById("loginForm")
-const user = document.getElementById("InputEmail")
-const pass = document.getElementById("InputPassword")
+const form = document.getElementById("loginForm");
+const user = document.getElementById("InputEmail");
+const pass = document.getElementById("InputPassword");
 
-form.addEventListener("submit", e=> {
-    e.preventDefault()
-    if(user.value.length <6){
-      alert("El nombre debe poseer al menos 6 caracteres")
-    }
-    if(pass.value.length <8){
-      alert("La contraseña debe poseer al menos 8 caracteres")
-    }
-})
+// Expresión regular para validar un correo electrónico
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Expresión regular para validar contraseña
+/*
+Al menos 8 caracteres de longitud.
+Al menos una letra mayúscula.
+Al menos un carácter especial. 
+Al meenos un numero del 0 al 9
+*/
+const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!emailRegex.test(user.value)) {
+    alert("Por favor, introduce un correo electrónico válido");
+  } else if (!passwordRegex.test(pass.value)) {
+    alert(
+      "La contraseña debe poseer al menos 8 caracteres, al menos una letra mayúscula al menos un carácter especial y al menos un numero del 0 al 9"
+    );
+    return false;
+  } else {
+    console.log(user.value.trim());
+    console.log(pass.value);
+    alert("credenciales correctas");
+  }
+});
